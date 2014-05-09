40d03596a609efcf202aab1ae9412577cd9b6b6a
Murat Ayfer <murat@ayfer.net>
2014-05-08 21:04:35 -0400

    ip number is templated

--- install.sh
+++ install.sh
@@ -3,7 +3,11 @@
 # assuming ran by root
 adduser --disabled-password --gecos "" demo
 cp -r demo /home/demo/
-apt-get install apache2 libapache2-mod-wsgi python-django
+apt-get -y install apache2 libapache2-mod-wsgi python-django
+
+IP=`wget -qO- http://ipecho.net/plain`
+sed -i "s/{ip}/$IP/g" demo.conf
+
 cp demo.conf /etc/apache2/sites-available/
 ln -s /etc/apache2/sites-available/demo.conf /etc/apache2/sites-enabled/demo.conf
 service apache2 restart
